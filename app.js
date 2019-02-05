d4737a0e37be637c5fae95446c3f4bdc374a7943
Lilian Jallageas <lilian.jallageas@free.fr>
2019-02-05 16:14:48 -0500

    master: Small cleanup.

--- app.js
+++ app.js
@@ -10,33 +10,12 @@ new Vue({
 	// Vue Data
 	// ===========
 	data: {
-		inputString: JSON.stringify({"firstName":"John","lastName":"Smith","isAlive":true,"age":27,"address":{"streetAddress":"21 2nd Street","city":"New York","state":"NY","postalCode":"10021-3100"},"phoneNumbers":[{"type":"home","number":"212 555-1234"},{"type":"office","number":"646 555-4567"},{"type":"mobile","number":"123 456-7890"}],"children":[],"spouse":null}),
+		inputString: "",
 		outputString: "",
 		indentation: "    ",
 		parseError: "",
 	},
 
-	// Mounted
-	// ===========
-	mounted:function(){
-		// jsonparse.parse('{"firstName":"John","lastName":"Smith","isAlive":true,"age":27,"address":{"streetAddress":"21 2nd Street","city":"New York","state":"NY","postalCode":"10021-3100"},"phoneNumbers":[{"type":"home","number":"212 555-1234"},{"type":"office","number":"646 555-4567"},{"type":"mobile","number":"123 456-7890"}],"children":[],"spouse":null}');
-		// jsonparse.parse('{"children": [],"spouse": null, "emptyObject": {} }');
-		// jsonparse.parse('  1234567890  ');
-		// jsonparse.parse('{\n"test_number" : 1234567890\n }');
-		// jsonparse.parse('123e-5');
-		// jsonparse.parse('null');
-		// jsonparse.parse('true');
-		// jsonparse.parse('false');
-		// jsonparse.parse('"test"');
-		// jsonparse.parse('{"test_A":"test_A"}');
-		// jsonparse.parse('{"test_A":"test_A"}');
-		// jsonparse.parse('\n{\n    "test_A" : "test_A",\n    "test_B" : "test_B"\n}');
-		// jsonparse.parse('\n[\n    "test_A"\n]\n');
-		// jsonparse.parse('["test_A",{"test_A":"test_A"},null]');
-		// jsonparse.parse('[true,false]');
-		// jsonparse.parse('[{"test_A":"test_A"},123456,null,false,{"array":[1234,"test"]}]');
-		
-	},
 
 	// Vue Methods
 	// ===========
